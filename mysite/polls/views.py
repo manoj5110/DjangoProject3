d5a4141478137062aeb6ef02c6347d6863531f60
manoj5110 <manojan19@gmail.com>
2022-07-06 12:37:48 +0530

    altered views with templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 
 def index(request):
     latest_question_list=Question.objects.order_by('pub_date')[:5]
-    output=','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # template_name=loader.get_template('polls/index.html')
+    context_name={'latest_question_list':latest_question_list}
+    # output=','.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(template_name.render(context_name,request))
+    return render(request,'polls/index.html',context_name)
 
 # Create your views here.
 def detail(request, question_id):
